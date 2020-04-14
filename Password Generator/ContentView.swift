cc9f8ddc954214f3ecc08385b5c282f5a7f2d741
chevalmuscle <me@marcantoinejean.com>
2020-04-13 22:20:28 -0400

    Initial Commit

---
+++ Password Generator/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Password Generator
+//
+//  Created by Marc-Antoine Jean on 2020-04-13.
+//  Copyright © 2020 Marc-Antoine Jean. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

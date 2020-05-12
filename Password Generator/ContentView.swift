7e3c7b23ec83fe508eb4f305dc5363edeccb7293
chevalmuscle <me@marcantoinejean.com>
2020-05-12 00:06:35 -0400

    added basic password generation

--- Password Generator/ContentView.swift
+++ Password Generator/ContentView.swift
@@ -9,10 +9,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    @State var password = "Password"
+    
+    var body: some View { VStack {
+        Text(password)
+            .frame(width: 140.0)
+        Button(action: {
+            self.password = getRandomPassword()
+            let pasteBoard = NSPasteboard.general
+            pasteBoard.clearContents()
+            pasteBoard.setString(self.password, forType: .string)
+        }) {
+        Text("Generate")
+        }
     }
+    .padding(20.0)
+    }
+}
+
+func getRandomPassword() -> String {
+    let len = 16
+    let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%?&*(){}[]"
+    let password = String((0..<len).compactMap{ _ in chars.randomElement() })
+    return password
 }
 
 

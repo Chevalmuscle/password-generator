3fda2a62fca84f1918a82c4ee2927707703c1eea
chevalmuscle <me@marcantoinejean.com>
2020-05-16 18:17:35 -0400

    added unit testing for password validation

--- Password Generator/ContentView.swift
+++ Password Generator/ContentView.swift
@@ -27,13 +27,59 @@ struct ContentView: View {
     }
 }
 
+/**
+ Enumation of errors for invalid parameters
+ */
+enum InvalidParameterError: Error {
+    /**
+     Integer has a value of less than 1
+     */
+    case NonPositiveIntError
+}
+
+/**
+ Generates a random password.
+ 
+ - Returns:
+    A strong password validated by the `isValidPassword(password: String, requiredLength: Int)` function
+ */
 func getRandomPassword() -> String {
-    let len = 16
+    let passwordLength = 16
     let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%?&*(){}[]"
-    let password = String((0..<len).compactMap{ _ in chars.randomElement() })
+    var password: String
+    
+    // makes sure the password is strong enough
+    repeat {
+        password = String((0..<passwordLength).compactMap{ _ in chars.randomElement() })
+    } while(try! !isValidPassword(password: password, requiredLength: passwordLength))
+    
     return password
 }
 
+/**
+ Checks if the password is strong enough.
+ 
+ - Parameters:
+    - password: the password that needs to be validated
+    - requiredLength: the length tha the password need to be
+ 
+ - Returns:
+    true if the password has:
+    - at least 2 upper case letters
+    - at least 1 special case letter (!@#$%?&*(){}[])
+    - at least 2 digits
+    - at least 3 lower case letters
+    - a length of requiredLength
+ */
+func isValidPassword(password: String, requiredLength: Int) throws -> Bool {
+    if (requiredLength < 1) {
+        throw InvalidParameterError.NonPositiveIntError
+    }
+    
+    // 2+ upper case letters, 1+ special case letter, 2+ digits, 3+ lower case letters, is of length 'requiredLength'
+    let passwordRegex = "^(?=.*[A-Z].*[A-Z])(?=.*[!@#$%?&*(){}\\[\\]])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{\(requiredLength)}$"
+    return password.range(of: passwordRegex, options: .regularExpression, range: nil, locale: nil) != nil ? true: false
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- Password GeneratorTests/Password_GeneratorTests.swift
+++ Password GeneratorTests/Password_GeneratorTests.swift
@@ -19,9 +19,46 @@ class Password_GeneratorTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testGetRandomPassword() throws {
+        
+        // the test is made by testing a lot of random password
+        // because getRandomPassword() returns a random output
+        
+        let strongPasswordRegex = "^(?=.*[A-Z].*[A-Z])(?=.*[!@#$%?&*(){}\\[\\]])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{16}$"
+        for _ in 1...20 {
+            XCTAssertTrue(getRandomPassword().range(of: strongPasswordRegex, options: .regularExpression, range: nil, locale: nil) != nil, "Strong enough password")
+        }
+        
+        let weakPasswordRegex = "^(?=.*[A-Z].*[A-Z])(?=.*[!@#$%?&*(){}\\[\\]])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$"
+        for _ in 1...20 {
+            XCTAssertFalse(getRandomPassword().range(of: weakPasswordRegex, options: .regularExpression, range: nil, locale: nil) != nil, "Weak password")
+        }
+    }
+    
+    func testIsValidPassword() throws {
+        // invalid length check
+        XCTAssertThrowsError(try isValidPassword(password: "9knD({&2y}&*od2L", requiredLength: 0)) { error in
+            XCTAssertEqual(error as! InvalidParameterError, InvalidParameterError.NonPositiveIntError)
+        }
+        XCTAssertThrowsError(try isValidPassword(password: "9knD({&2y}&*od2L", requiredLength: -1)) { error in
+            XCTAssertEqual(error as! InvalidParameterError, InvalidParameterError.NonPositiveIntError)
+        }
+        
+        // valid password check
+        XCTAssertTrue(try isValidPassword(password: "9knD({&2y}&*od2L", requiredLength: 16), "Valid password")
+        XCTAssertTrue(try isValidPassword(password: "LA#Ds9Xx)o02&iYU", requiredLength: 16), "Valid password")
+        XCTAssertTrue(try isValidPassword(password: "AB!12cde", requiredLength: 8), "Valid password")
+        
+        // required characters check
+        XCTAssertFalse(try isValidPassword(password: "AB!12cde", requiredLength: 7), "Invalid password")
+        XCTAssertFalse(try isValidPassword(password: "A!12cde", requiredLength: 7), "Invalid password")
+        XCTAssertFalse(try isValidPassword(password: "AB12cde", requiredLength: 7), "Invalid password")
+        XCTAssertFalse(try isValidPassword(password: "AB!1cde", requiredLength: 7), "Invalid password")
+        XCTAssertFalse(try isValidPassword(password: "AB!12cd", requiredLength: 7), "Invalid password")
+        
+        // wrong password length check
+        XCTAssertFalse(try isValidPassword(password: "", requiredLength: 7), "Invalid password")
+        XCTAssertFalse(try isValidPassword(password: "AB!12cde", requiredLength: 2), "Invalid password")
     }
 
     func testPerformanceExample() throws {
